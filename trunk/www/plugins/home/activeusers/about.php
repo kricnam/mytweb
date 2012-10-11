d7a18290cb64853a7161ef0c2c9d72f4ded382ba
wyjbupt@gmail.com <wyjbupt@gmail.com@77e89820-a481-9fc3-fdb5-4cb0dc0fe799>
2012-10-11 12:01:02 +0000

    新增首页插件

---
+++ trunk/www/plugins/home/activeusers/about.php
@@ -0,0 +1,13 @@
+<?php
+defined('IN_TS') or die('Access Denied.');
+//活跃用户
+return array(
+'name' => '活跃用户',
+'version' => '1.0',
+'desc' => '活跃用户',
+'url' => 'http://www.vooga.cn',
+'email' => '635434924@qq.com',
+'author' => '王贤光',
+'author_url' => 'http://www.vooga.cn',
+'isedit'	=> '0',
+);
---
+++ trunk/www/plugins/home/article/about.php
@@ -0,0 +1,13 @@
+<?php
+defined('IN_TS') or die('Access Denied.');
+//插件信息
+return array(
+'name' => '首页最新资讯',
+'version' => '1.0',
+'desc' => '首页最新资讯',
+'url' => 'http://www.thinksaas.cn',
+'email' => 'thinksaas@qq.com',
+'author' => '邱君',
+'author_url' => 'http://www.thinksaas.cn',
+'isedit'	=> '0',
+);
