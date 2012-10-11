928727d4915501020a89d3e2feb421dfd3e343df
wyjbupt@gmail.com <wyjbupt@gmail.com@77e89820-a481-9fc3-fdb5-4cb0dc0fe799>
2012-10-11 11:59:47 +0000

    一些数据文件（可有可无）

---
+++ trunk/www/data/study_types.php
@@ -0,0 +1,68 @@
+<?php
+defined('IN_TS') or die('Access Denied.');
+return array (
+  'list' => 
+  array (
+    0 => 
+    array (
+      'typeid' => '1',
+      'typename' => '公开课',
+      'count_study' => '1',
+    ),
+    1 => 
+    array (
+      'typeid' => '2',
+      'typename' => '职业认证',
+      'count_study' => '1',
+    ),
+    2 => 
+    array (
+      'typeid' => '3',
+      'typename' => '商业职场',
+      'count_study' => '0',
+    ),
+    3 => 
+    array (
+      'typeid' => '4',
+      'typename' => '计算机与网络',
+      'count_study' => '1',
+    ),
+    4 => 
+    array (
+      'typeid' => '5',
+      'typename' => '生活',
+      'count_study' => '0',
+    ),
+    5 => 
+    array (
+      'typeid' => '6',
+      'typename' => '语言',
+      'count_study' => '0',
+    ),
+    6 => 
+    array (
+      'typeid' => '7',
+      'typename' => '艺术',
+      'count_study' => '0',
+    ),
+    7 => 
+    array (
+      'typeid' => '8',
+      'typename' => '体育',
+      'count_study' => '0',
+    ),
+    8 => 
+    array (
+      'typeid' => '9',
+      'typename' => '中小学教育',
+      'count_study' => '0',
+    ),
+    9 => 
+    array (
+      'typeid' => '10',
+      'typename' => '其他',
+      'count_study' => '0',
+    ),
+  ),
+  'count' => '3',
+);
