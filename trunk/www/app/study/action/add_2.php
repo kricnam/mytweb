6ceb49813e7caf0136ef2fe1a6fbb1ac90dff453
me.xinxin@gmail.com <me.xinxin@gmail.com@77e89820-a481-9fc3-fdb5-4cb0dc0fe799>
2012-10-21 14:23:07 +0000

    uri adjust

--- trunk/www/app/study/action/add_2.php
+++ trunk/www/app/study/action/add_2.php
@@ -3,7 +3,7 @@ defined('IN_TS') or die('Access Denied.');
 
 $userid = intval($TS_USER['user']['userid']);
  
-if($userid =='0') header("Location: ".SITE_URL."index.php/user/login");
+if($userid =='0') header("Location: ".SITE_URL."index.php?app=user&ac=login");
 
 $groupid = isset($_GET['groupid']) ? $_GET['groupid'] : '0';
 $studyid = isset($_GET['studyid']) ? $_GET['studyid'] : '';
