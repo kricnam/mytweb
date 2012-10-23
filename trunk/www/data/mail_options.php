9aa3e3e575169c026fa395945dd2dcb99afa5caa
me.xinxin@gmail.com <me.xinxin@gmail.com@77e89820-a481-9fc3-fdb5-4cb0dc0fe799>
2012-10-23 02:43:37 +0000

    remove site depend config file

--- trunk/www/data/mail_options.php
+++ trunk/www/data/mail_options.php
@@ -3,9 +3,9 @@ defined('IN_TS') or die('Access Denied.');
 return array (
   'appname' => '邮件',
   'appdesc' => 'ThinkSAAS邮件',
-  'isenable' => '0',
-  'mailhost' => 'smtp.exmail.qq.com',
+  'isenable' => '1',
+  'mailhost' => '127.0.0.1',
   'mailport' => '25',
-  'mailuser' => 'admin@thinksaas.cn',
-  'mailpwd' => '123456',
+  'mailuser' => 'root',
+  'mailpwd' => '',
 );
